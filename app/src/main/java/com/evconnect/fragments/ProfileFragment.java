da9b9449204a73287f099e6c6e1190340b2773a9
IT20264352-Amarasinghe <it20264352@my.sliit.lk>
2025-09-21 16:30:17 +0530

    Offline feature created for user details

--- app/src/main/java/com/evconnect/fragments/ProfileFragment.java
+++ app/src/main/java/com/evconnect/fragments/ProfileFragment.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,6 +15,7 @@ import androidx.fragment.app.Fragment;
 
 import com.evconnect.R;
 import com.evconnect.activities.LoginActivity;
+import com.evconnect.db.UserDao;
 import com.evconnect.models.UserInfo;
 import com.evconnect.utils.JwtUtils;
 import com.evconnect.utils.TokenManager;
@@ -21,6 +23,8 @@ import com.evconnect.utils.TokenManager;
 public class ProfileFragment extends Fragment {
 
     private TokenManager tokenManager;
+
+    UserInfo user;
 
     @Nullable
     @Override
@@ -40,12 +44,28 @@ public class ProfileFragment extends Fragment {
         String token = tokenManager.getToken();
 
         if (token != null) {
-            UserInfo user = JwtUtils.extractUserInfo(token);
-
+            user = JwtUtils.extractUserInfo(token);
             tvName.setText(user.name);
             tvNic.setText(user.nic);
             tvEmail.setText(user.email);
             tvRole.setText(user.role);
+
+        }else{
+            String currentNic = tokenManager.getCurrentUserNic();
+            if (currentNic != null) {
+                UserDao userDao = new UserDao(requireContext());
+                user = userDao.getUserByNic(currentNic);
+                if(user != null){
+                    tvName.setText(user.name);
+                    tvNic.setText(user.nic);
+                    tvEmail.setText(user.email);
+                    tvRole.setText(user.role);
+                }else{
+                    Toast.makeText(getContext(), "User not found on offline. Please login using internet.", Toast.LENGTH_SHORT).show();
+                }
+            }else{
+                Toast.makeText(getContext(), "Please login with internet. User name not found", Toast.LENGTH_SHORT).show();
+            }
         }
 
         btnLogout.setOnClickListener(v -> {
